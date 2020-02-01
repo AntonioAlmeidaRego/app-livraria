baf54bd0023bed95bb12f09ff9c3e0cc6b75dd57
Antonio <antonio.alm_pdf@hotmail.com>
2020-02-01 13:53:58 -0300

    Criando a SessionController.js

---
+++ livraria/livraria-app/app/controllers/SessionController.js
@@ -0,0 +1,26 @@
+
+import {AsyncStorage} from 'react-native';
+
+export default class SessionController {
+
+    criarSession = async (key, objeto) => {
+        const session = await AsyncStorage.getItem(key);
+        if(session != null){
+            await AsyncStorage.removeItem(key);
+        }
+        await AsyncStorage.setItem(key,
+            JSON.stringify(objeto));
+    };
+
+    getSession = async (key) => {
+        return await JSON.parse(await AsyncStorage.getItem(key)) || [];
+    };
+
+    removeSession = async (key)=>{
+        await AsyncStorage.removeItem(key);
+    };
+
+    clearSessions = async ()=>{
+        await AsyncStorage.clear();
+    };
+}
